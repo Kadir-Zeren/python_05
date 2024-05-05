bdbea7e7e6518cf51c0f783cbe5765edaf921cd1
kadir-zeren <k4dzeren@gmail.com>
2024-05-05 22:34:50 +0300

    first commit

---
+++ python.py
@@ -0,0 +1,25 @@
+text = 'Clarusway'
+print(text[0])
+
+print(text[:5] + 'k' + text[-3:])
+
+text='AaBbCc'
+print(text.lower())
+
+text = text.lower()
+print(text)
+
+var_str = 'In God we Trust'
+print(var_str.lower())
+
+cumle = 'In God We Trust'
+print(cumle.lower())
+
+print('t' in cumle)
+print('R' in cumle)
+print('trust' in cumle)
+print('R' not in cumle)
+
+text = 'www.clarusway.com'
+print(text.endswith('.com'))
+print(text.startswith('http:'))
